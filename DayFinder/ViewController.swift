930104ec02a4d5b2fb9e1e40fa22949486db5b47
Artjoms Vorona <artjomsvorona@gmail.com>
2019-10-28 11:59:37 +0200

    Initial UI, IBOutlets and IBAction connections

--- DayFinder/ViewController.swift
+++ DayFinder/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dayTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    
+    @IBOutlet var dateTextFields: [UITextField]!
+    
+    @IBOutlet weak var dayOfWeekLabel: UILabel!
+    
+    @IBOutlet weak var findButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setInitialViewSettings()
     }
 
-
+    func setInitialViewSettings() {
+        let navBarAppearence = UINavigationBarAppearance()
+        navBarAppearence.backgroundColor = .systemGray
+        navBarAppearence.largeTitleTextAttributes =  [NSAttributedString.Key.foregroundColor : UIColor.systemOrange]
+        self.navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearence
+        
+        self.view.backgroundColor = .systemGray2
+        
+        for textField in dateTextFields {
+            textField.backgroundColor = .systemGray5
+            textField.clearButtonMode = .always
+        }
+        
+        findButton.backgroundColor = .systemGray5
+        findButton.layer.cornerRadius = 4
+        findButton.layer.borderColor = UIColor.white.cgColor
+        findButton.layer.borderWidth = 2
+        findButton.setTitleColor(.systemOrange, for: .normal)
+    }
+    
+    
+    @IBAction func findButtonTouched(_ sender: UIButton) {
+    }
+    
 }
 
